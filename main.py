5aef092502f41dd1d7dd9143442ae266367c8ea0
pablodob <pablo.dob@gmail.com>
2018-07-15 16:51:45 -0300

    Corrections

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import load_data as ld
 import tensorflow as tf
+import numpy as np
 
 # Load data
 data = ld.load_data()
 
 labels_data=data[:,0:1]
-inputs_data=data[:,1:len(data[0])]
+inputs_data=data[:,2:len(data[0])]
 
 # Variable definition
 l = 0.01
-epoch = 100
+epoch = 25
 batch_size = 20
 n_train = int(len(labels_data)*.9)
 
@@ -35,14 +36,13 @@ inputs = tf.placeholder(shape=(None,len(inputs_train[0])), dtype=tf.float32, nam
 # Labels
 labels = tf.placeholder(shape=(None,1), dtype=tf.float32, name="labels")
 
+# Building the model
+input_layer = tf.contrib.layers.fully_connected(inputs=inputs, num_outputs = 25, activation_fn = tf.nn.relu)
+hidden_layer = tf.contrib.layers.fully_connected(inputs=input_layer, num_outputs = 5, activation_fn = tf.nn.relu)
+outputs = tf.contrib.layers.fully_connected(inputs=hidden_layer, num_outputs = 1, activation_fn = tf.nn.sigmoid)
+
 # Initializater TensorFlow
 init = tf.global_variables_initializer()
-
-# Building the model
-input_layer = tf.contrib.layers.fully_connected(inputs=inputs, num_outputs = 100, activation_fn = tf.nn.sigmoid)
-hidden1_layer = tf.contrib.layers.fully_connected(inputs=input_layer, num_outputs = 25, activation_fn = tf.nn.sigmoid)
-hidden2_layer = tf.contrib.layers.fully_connected(inputs=hidden1_layer, num_outputs = 5, activation_fn = tf.nn.sigmoid)
-outputs = tf.contrib.layers.fully_connected(inputs=hidden2_layer, num_outputs = 1, activation_fn = tf.nn.sigmoid)
 
 # Cost and Optimizer
 with tf.name_scope("Cost_and_Optimizer"):
@@ -63,24 +63,24 @@ with tf.Session() as sess:
         for j in range(num_data):
             sess.run(train, feed_dict={inputs: list_inputs_train[j], labels: list_labels_train[j]})
 
-    #To calcule acurracy and cost
-    if i%10:
-        partial_cost = sess.run(cost, feed_dict={inputs: inputs_train, labels: labels_train})
-        outputs = sess.run(output, feed_dict={inputs: inputs_train, labels: labels_train})
-        predictions = outputs > 0.5
+        #To calcule acurracy and cost
+        if i%10==0:
+            partial_cost = sess.run(cost, feed_dict={inputs: inputs_train, labels: labels_train})
+            result = sess.run(outputs, feed_dict={inputs: inputs_train, labels: labels_train})
+            predictions = result > 0.5
 
-        accuracy = np.sum(labels_train == predictions) / len(labels_train)
+            accuracy = np.sum(labels_train == predictions) / len(labels_train)
 
-        print("Train: Epoch N°: {} - Cost = {} - Accuracy = {}%".format(i, partial_cost, accuracy * 100))
+            print("Train: Epoch N°: {} - Cost = {} - Accuracy = {}%".format(i, partial_cost, accuracy * 100))
 
     s = sess.run(merged_summary, feed_dict={inputs: inputs_train, labels: labels_train})
     writer.add_summary(s, i)
 
-# Print cost test and final acurracy
-final_cost = sess.run(cost, feed_dict={x: inputs_test, y: labels_test})
-outputs = sess.run(output, feed_dict={x: inputs_test, y: labels_test})
-predictions = outputs > 0.5
+    # Print cost test and final acurracy
+    final_cost = sess.run(cost, feed_dict={inputs: inputs_test, labels: labels_test})
+    outputs = sess.run(outputs, feed_dict={inputs: inputs_test, labels: labels_test})
+    predictions = outputs > 0.5
 
-final_accuracy = np.sum(labels_test == predictions) / len(labels_test)
+    final_accuracy = np.sum(labels_test == predictions) / len(labels_test)
 
 print("Test: Cost = {} - Accuracy = {}%".format(final_cost, final_accuracy* 100))
